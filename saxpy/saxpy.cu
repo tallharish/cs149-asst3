c67366644ce9dc21bed7539242875bb9742f0398
Ubuntu <ubuntu@ip-172-31-13-53.us-west-2.compute.internal>
2024-10-30 22:45:25 +0000

    part1 - formating

--- saxpy/saxpy.cu
+++ saxpy/saxpy.cu
@@ -6,17 +6,17 @@
 
 #include "CycleTimer.h"
 
-
 // return GB/sec
-float GBPerSec(int bytes, float sec) {
-  return static_cast<float>(bytes) / (1024. * 1024. * 1024.) / sec;
+float GBPerSec(int bytes, float sec)
+{
+    return static_cast<float>(bytes) / (1024. * 1024. * 1024.) / sec;
 }
-
 
 // This is the CUDA "kernel" function that is run on the GPU.  You
 // know this because it is marked as a __global__ function.
 __global__ void
-saxpy_kernel(int N, float alpha, float* x, float* y, float* result) {
+saxpy_kernel(int N, float alpha, float *x, float *y, float *result)
+{
 
     // compute overall thread index from position of thread in current
     // block, and given the block we are in (in this example only a 1D
@@ -24,13 +24,11 @@ saxpy_kernel(int N, float alpha, float* x, float* y, float* result) {
     // blockDim and threadIdx.
     int index = blockIdx.x * blockDim.x + threadIdx.x;
 
-
     // this check is necessary to make the code work for values of N
     // that are not a multiple of the thread block size (blockDim.x)
     if (index < N)
-       result[index] = alpha * x[index] + y[index];
+        result[index] = alpha * x[index] + y[index];
 }
-
 
 // saxpyCuda --
 //
@@ -38,7 +36,8 @@ saxpy_kernel(int N, float alpha, float* x, float* y, float* result) {
 // memory on the GPU using CUDA API functions, uses CUDA API functions
 // to transfer data from the CPU's memory address space to GPU memory
 // address space, and launches the CUDA kernel function on the GPU.
-void saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultarray) {
+void saxpyCuda(int N, float alpha, float *xarray, float *yarray, float *resultarray)
+{
 
     // must read both input arrays (xarray and yarray) and write to
     // output array (resultarray)
@@ -62,10 +61,10 @@ void saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultar
     // above) but you cannot access the contents these buffers from
     // this thread. CPU threads cannot issue loads and stores from GPU
     // memory!
-    float* device_x = nullptr;
-    float* device_y = nullptr;
-    float* device_result = nullptr;
-    
+    float *device_x = nullptr;
+    float *device_y = nullptr;
+    float *device_result = nullptr;
+
     //
     // CS149 TODO: allocate device memory buffers on the GPU using cudaMalloc.
     //
@@ -75,7 +74,7 @@ void saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultar
     //
     // https://devblogs.nvidia.com/easy-introduction-cuda-c-and-c/
     //
-        
+
     // start timing after allocation of device memory
     double startTime = CycleTimer::currentSeconds();
 
@@ -83,7 +82,6 @@ void saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultar
     // CS149 TODO: copy input arrays to the GPU using cudaMemcpy
     //
 
-   
     // run CUDA kernel. (notice the <<< >>> brackets indicating a CUDA
     // kernel launch) Execution on the GPU occurs here.
     saxpy_kernel<<<blocks, threadsPerBlock>>>(N, alpha, device_x, device_y, device_result);
@@ -92,14 +90,14 @@ void saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultar
     // CS149 TODO: copy result from GPU back to CPU using cudaMemcpy
     //
 
-    
     // end timing after result has been copied back into host memory
     double endTime = CycleTimer::currentSeconds();
 
     cudaError_t errCode = cudaPeekAtLastError();
-    if (errCode != cudaSuccess) {
+    if (errCode != cudaSuccess)
+    {
         fprintf(stderr, "WARNING: A CUDA error occured: code=%d, %s\n",
-		errCode, cudaGetErrorString(errCode));
+                errCode, cudaGetErrorString(errCode));
     }
 
     double overallDuration = endTime - startTime;
@@ -108,10 +106,10 @@ void saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultar
     //
     // CS149 TODO: free memory buffers on the GPU using cudaFree
     //
-    
 }
 
-void printCudaInfo() {
+void printCudaInfo()
+{
 
     // print out stats about the GPU in the machine.  Useful if
     // students want to know what GPU they are running on.
@@ -122,7 +120,8 @@ void printCudaInfo() {
     printf("---------------------------------------------------------\n");
     printf("Found %d CUDA devices\n", deviceCount);
 
-    for (int i=0; i<deviceCount; i++) {
+    for (int i = 0; i < deviceCount; i++)
+    {
         cudaDeviceProp deviceProps;
         cudaGetDeviceProperties(&deviceProps, i);
         printf("Device %d: %s\n", i, deviceProps.name);
